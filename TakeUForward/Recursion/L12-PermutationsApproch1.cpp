dafe6c121879d4243a4414638354e0d4d392f954
ZacHolme7 <zacholme@gmail.com>
2023-05-22 12:06:32 -0500

    update

--- TakeUForward/Recursion/L12-PermutationsApproch1.cpp
+++ TakeUForward/Recursion/L12-PermutationsApproch1.cpp
@@ -3,6 +3,12 @@
 // Date solved: May 17, 2023
 /*
 Solution:
+We want to take all the possible permutations of the given input list. this is different from subsets. Each idx can be a starting. We follow the same pick not
+pick structure with a twist. We want to maintain a list of the ones that we have picked and that we have not picked. This will make it so that we dont 
+take the same permutation starting with the same parent. For example, if we have the list [1, 2, 3] and we are starting at 2, we want to mark 2 as picked so that 
+when we recurse and start the iteration again, we can pick 1. Here again we mark this as picked and we can take three on the next iteration, we can then unmark 1
+as picked, we see that 2 is picked, and then we can mark 3 and pick it. on the next call we will see 1 is not picked so we can pick that and get [2, 3, 1]. We then 
+see that the rest are picked and we have got all the numbers that we can. 
 */
 
 #include <vector>
