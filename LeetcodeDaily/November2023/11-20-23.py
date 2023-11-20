4eaed009aac433086f764e3e8d0e36d9fe568ca3
ZacHolme7 <zacholme@gmail.com>
2023-11-20 09:48:38 -0600

    added some comments

--- LeetcodeDaily/November2023/11-20-23.py
+++ LeetcodeDaily/November2023/11-20-23.py
@@ -7,18 +7,29 @@ class Solution:
         total = 0
         n = len(garbage)
 
+        # go through all of the garbage types
         for c in "MPG":
             mx = 0
             current = 0
 
+            # count for the first location
             current += garbage[0].count(c)
             mx = max(current, mx)
 
+            # go through the rest of the locations
             for i in range(1, n):
-                current += travel[i - 1]
+                current += travel[i - 1] # add the travel distance
+
+                # if there is garbage at this location
                 if c in garbage[i]:
+                    # count the amount of garbage and add it to the current
                     current += garbage[i].count(c)
+
+                    # keep track of the max to add to the total at the end
+                    # this will track the cost up to the current location
                     mx = max(current, mx)
+
+            # add the max for the specified type to the total
             total += mx
 
         return total
