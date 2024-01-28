53090d2d53dc8db0a38a83a23657a45ffc38c94d
ZacHolme7 <zacholme@gmail.com>
2024-01-28 09:48:13 -0600

    daily problems

---
+++ LeetcodeDaily/January2024/1-28-24.py
@@ -0,0 +1,22 @@
+# link: https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/description/?envType=daily-question&envId=2024-01-28
+# difficulty: hardk
+# date solved: january 28, 2024
+# did not solve on my own again...
+
+
+class Solution:
+    def numSubmatrixSumTarget(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        for x in range(m):
+            for y in range(n - 1):
+                matrix[x][y + 1] += matrix[x][y]
+        res = 0
+        for y1 in range(n):
+            for y2 in range(y1, n):
+                preSums = {0: 1}
+                s = 0
+                for x in range(m):
+                    s += matrix[x][y2] - (matrix[x][y1 - 1] if y1 > 0 else 0)
+                    res += preSums.get(s - target, 0)
+                    preSums[s] = preSums.get(s, 0) + 1
+        return res
