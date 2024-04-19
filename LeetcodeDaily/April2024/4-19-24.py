b2d4accdc3203b72316d2d3c0083643ca45f3ad5
ZacHolme7 <zacholme@gmail.com>
2024-04-19 08:52:07 -0500

    update

---
+++ LeetcodeDaily/April2024/4-19-24.py
@@ -0,0 +1,34 @@
+# link: https://leetcode.com/problems/number-of-islands/?envType=daily-question&envId=2024-04-19
+# difficulty: medium
+# date solved: april 19, 2024
+
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = [(r, c)]
+            visited.add((r, c))
+            while q:
+                row, col = q.pop(0)
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                            (r, c) not in visited):
+                        q.append((r, c))
+                        visited.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    islands += 1
+        return islands
