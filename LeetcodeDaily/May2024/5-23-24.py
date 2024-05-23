8c745981cffbf79ab1570b520fee74e6a0322779
Zachary Holme <zacholme@gmail.com>
2024-05-23 09:11:33 -0500

    update

---
+++ LeetcodeDaily/May2024/5-23-24.py
@@ -0,0 +1,19 @@
+# link: https://leetcode.com/problems/the-number-of-beautiful-subsets/?envType=daily-question&envId=2024-05-232
+# difficulty: medium
+# date solved: May 22, 2024
+
+class Solution:
+    def beautifulSubsets(self, nums, k):
+        freq = {}
+        def f(i: int) -> int:
+            if i == len(nums): # base case
+                return 1
+            result = f(i + 1) # nums[i] not taken
+            if not nums[i] - k in freq and not nums[i] + k in freq: # check if we can take nums[i]
+                freq[nums[i]] = freq.get(nums[i], 0) + 1
+                result += f(i + 1) # nums[i] taken
+                freq[nums[i]] -= 1
+                if freq[nums[i]] == 0:
+                    del freq[nums[i]]
+            return result
+        return f(0) - 1 # -1 for empty subset
