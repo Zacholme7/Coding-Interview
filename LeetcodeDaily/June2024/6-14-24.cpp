2019120430e21da9defa115f4cc17527bc2bb841
Zachary Holme <zacholme@gmail.com>
2024-06-14 12:00:00 -0500

    daily

---
+++ LeetcodeDaily/June2024/6-14-24.cpp
@@ -0,0 +1,18 @@
+// link: https://leetcode.com/problems/minimum-increment-to-make-array-unique/description/?envType=daily-question&envId=2024-06-14
+// difficulty: medium
+// date solved: june 14, 2024
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0, need = 0;
+        for (int num: nums) {
+            res += max(need - num, 0);
+            need = max(num, need)+1;
+        }
+        return res;
+    }
+};
