91b08a9b4ba4cd04bd3b5419f4b933bca1c03219
ZacHolme7 <zacholme@gmail.com>
2024-06-23 16:15:59 -0500

    daily

---
+++ LeetcodeDaily/June2024/6-23-24.py
@@ -0,0 +1,33 @@
+# link: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/description/?envType=daily-question&envId=2024-06-23
+# difficulty: medium
+# date solved: june 23, 2024
+
+import deque
+
+
+class Solution(object):
+    def longestSubarray(self, nums, limit):
+        increase = deque()
+        decrease = deque()
+        max_length = 0
+        left = 0
+
+        for right in range(len(nums)):
+            while increase and nums[right] < increase[-1]:
+                increase.pop()
+            increase.append(nums[right])
+
+            while decrease and nums[right] > decrease[-1]:
+                decrease.pop()
+            decrease.append(nums[right])
+
+            while decrease[0] - increase[0] > limit:
+                if nums[left] == increase[0]:
+                    increase.popleft()
+                if nums[left] == decrease[0]:
+                    decrease.popleft()
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
