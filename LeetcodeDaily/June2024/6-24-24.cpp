6289bba6ceb7ed4fa3eb07db87884da18c03448d
ZacHolme7 <zacholme@gmail.com>
2024-06-24 12:26:49 -0500

    daily problem

---
+++ LeetcodeDaily/June2024/6-24-24.cpp
@@ -0,0 +1,29 @@
+// link: https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/description/?envType=daily-question&envId=2024-06-24
+// difficulty: hard
+// date solved june 24, 2024
+
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int flipped = 0;
+        int res = 0;
+        vector<int> isFlipped(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (i >= k) {
+                flipped ^= isFlipped[i - k];
+            }
+            if (flipped == nums[i]) {
+                if (i + k > n) {
+                    return -1;
+                }
+                isFlipped[i] = 1;
+                flipped ^= 1;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
